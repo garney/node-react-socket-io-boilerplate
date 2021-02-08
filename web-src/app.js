030890827c1acb12767bff89bc5689773a5e54f4
garney <garney@gmail.com>
2021-02-09 08:18:55 +1100

    initiialise socket on index to avoid any React updates handling

--- web-src/app.js
+++ web-src/app.js
@@ -1,28 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import Config from './config';
-import Socket from './socket/index';
 
 import './app.scss';
 
 import Dice from './dice';
 
-function App() {
-    const [socket, setSocket] = useState({});
+function App({socket}) {
+    
     const [connectionDetails, setConnectionDetails] = useState({});
 
     useEffect(() => {
-        Config.getConfig().then(config => {
-            if(config.socketUrl) {
-                const socket = new Socket(config.socketUrl);
-                socket.on('connected', (id) => {
-                    setConnectionDetails({
-                        id,
-                        status: socket.status
-                    })
-                });
-                setSocket(socket);
-            }
-        })
+        socket.on('connected', (id) => {
+            setConnectionDetails({
+                id,
+                status: socket.status
+            })
+        });
+    
 
     }, []);
 
--- web-src/index.js
+++ web-src/index.js
@@ -1,19 +1,25 @@
 import 'babel-polyfill';
+import Socket from './socket/index';
+import Config from './config';
 
 import ReactDOM from 'react-dom';
 import React from 'react';
 
 import App from './app';
 
+
 // import './style/main.scss'
-const init = () => {
+const init = (config) => {
+    const socket = new Socket(config.socketUrl);
     ReactDOM.render(
-    <App env={{test:'test'}} />
+    <App env={{test:'test'}} socket={socket} />
         ,
         document.getElementById('root'));
 };
 
-init();
+Config.getConfig().then(config => {
+    init(config);
+});
 
 // Hot Module Replacement
 if (module.hot) {
