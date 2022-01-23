aa0261eedc2b04571482a1064c0f72bf111ebd3a
Garney Andres <garney.a@hanasoft.com.au>
2022-01-24 09:21:41 +1100

    update

--- server-src/socket.js
+++ server-src/socket.js
@@ -1,27 +1,54 @@
 
 import shortId from 'shortid';
 export default class Socket {
-  constructor(col) {
+  constructor(socket) {
+    this.socket = socket;
+    // console.log('connection established');
+    console.log(socket.id, 'CONNECTED')
+    socket.emit(`connected`, shortId.generate());
+    this.setup();
+    
+  }
 
+  roll = (num) => {
+      console.log('roll');
+      const dice = [];
+      for(let i = 0; i < num; i++) {
+        dice.push(Math.ceil(Math.random()*6))
+      }
+      console.log('rolled', dice)
+      this.socket.emit('rolled', dice)
+  }
+  setup = () => {
+    console.log('setup');
+    this.socket.on('disconnect', this.onDisconnect);
+    this.socket.on('reconnect', this.onReconnect);
+
+    //
+    this.socket.on('roll', this.roll)
+  }
+  destroy = () => {
+    console.log('destroy');
+    this.socket.off('disconnect', this.onDisconnect);
+    this.socket.off('reconnect', this.onReconnect);
+    this.socket.off('roll', this.roll);
+  }
+
+  onDisconnect = () => {
+    console.log('disconnect');
+    this.destroy();
+  }
+  onReconnect = () => {
+    console.log('reconnect');
+    this.setup();
   }
 
   static init(server) {
 
     const io = require('socket.io')(server);
-    io.on('connection', function(socket){
-      console.log('connection established');
-      socket.emit(`connected`, shortId.generate());
-      socket.on('disconnect', function(){
-        // logger.info(`session disconnected: test`);
-      });
-
-      socket.on('roll', (num) => {
-        const dice = [];
-        for(let i = 0; i < num; i++) {
-          dice.push(Math.ceil(Math.random()*6))
-        }
-        socket.emit('rolled', dice)
-      })
+    Socket.io = io;
+    io.on('connection', (socket) => {
+      new Socket(socket);
     });
     return io;
   }
--- web-src/app.js
+++ web-src/app.js
@@ -1,28 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import Config from './config';
-import Socket from './socket/index';
 
 import './app.scss';
 
 import Dice from './dice';
 
-function App() {
-    const [socket, setSocket] = useState({});
+function App({socket,connectionId}) {
+    
     const [connectionDetails, setConnectionDetails] = useState({});
 
     useEffect(() => {
-        Config.getConfig().then(config => {
-            if(config.socketUrl) {
-                const socket = new Socket(config.socketUrl);
-                socket.on('connected', (id) => {
-                    setConnectionDetails({
-                        id,
-                        status: socket.status
-                    })
-                });
-                setSocket(socket);
-            }
-        })
+        socket.on('connected', (id) => {
+            setConnectionDetails({
+                id,
+                status: socket.status
+            })
+        });
+    
 
     }, []);
 
@@ -31,7 +25,7 @@ function App() {
         <div className="app">
             <div>
                 <span className="status">{connectionDetails.status}</span> with
-                connection ID <span className="id">{connectionDetails.id}</span>
+                connection ID <span className="id">{connectionId}</span>
             </div>
             <Dice socket={socket}/>
         </div>
